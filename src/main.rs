577df6bce96cca666a1ec04feea5d00d61e56415
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-05-21 23:21:22 +0200

    Improving with example text and scroll

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use ratatui::{prelude::*, widgets::*};
-use std::{io::{self, BufRead, BufReader}, process::{Command, Stdio}, sync::mpsc, thread, time::Duration};
+use core::panic;
+use std::{fmt::Error, io::{self, BufRead, BufReader, ErrorKind}, process::{Command, Stdio}, sync::mpsc, thread, time::Duration};
 use crossterm::{event::{self, Event, KeyCode}, execute, terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}};
 use std::io::stdout;
 
-fn main() -> io::Result<()> {
+
+
+
+
+fn main1() -> Result<(), Box<dyn std::error::Error>> {
     // Channel for communication between reader thread and UI
     let (tx, rx) = mpsc::channel();
 
@@ -42,8 +47,11 @@ fn main() -> io::Result<()> {
     execute!(stdout(), EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout());
     let mut terminal = Terminal::new(backend)?;
+    let mut scroll: u16 = 0;
 
     let mut lines: Vec<String> = Vec::new();
+    let mut running = String::new();
+    let mut last_text = String::new();
 
     loop {
         if event::poll(Duration::from_millis(100))? {
@@ -57,11 +65,50 @@ fn main() -> io::Result<()> {
 
                     return Ok(());
                 }
+                if key.code == KeyCode::Down {
+                    scroll = scroll.saturating_add(1);
+                }
+                if key.code == KeyCode::Up {
+                    scroll = scroll.saturating_sub(1);
+                }
             }
         }
 
         // Non-blocking receive
         while let Ok(line) = rx.try_recv() {
+
+            let mut chars = line.chars();
+            chars.next();
+            chars.next_back();
+            let [title, artist, album, length, position]: [&str; 5] = chars.as_str()
+                .split('|').collect::<Vec<&str>>().try_into().unwrap();
+            
+            let mut perc:f64 = 0.0;
+            if let (Ok(ipos), Ok(ilen)) = (position.parse::<f64>(), length.parse::<f64>()) {
+                perc = ipos / ilen;
+            }
+
+            let new_running = format!("{title} {artist}");
+            if running != new_running {
+                running = new_running;
+                // f.set_title(&running);
+                let rt = tokio::runtime::Runtime::new()?;
+                let text = rt.block_on(downloadLyrics());
+
+                match text {
+                    Ok(lyric_finder::LyricResult::Some {
+                        track,
+                        artists,
+                        lyric,
+                    }) => {
+                        last_text = lyric;
+                        // println!("{} by {}'s lyric:\n{}", track, artists, lyric)
+                    },
+                    Ok(lyric_finder::LyricResult::None) => { last_text = "".to_string(); println!("lyric not found!") },
+                    Err(e) => { last_text = "".to_string(); println!("Error: {}", e) }
+                }
+            }
+
             lines.push(line);
             if lines.len() > 10 {
                 lines.remove(0);
@@ -84,12 +131,41 @@ fn main() -> io::Result<()> {
                 if let (Ok(ipos), Ok(ilen)) = (position.parse::<f64>(), length.parse::<f64>()) {
                     perc = ipos / ilen;
                 }
-                let to_print = format!("title: {title}\nartist {artist}\nalbum {album}\nlength {length}\nposition {position}\npercentage {perc}");
-                let paragraph = Paragraph::new(to_print).block(block);
+                let to_print = format!("title: {title}\nartist {artist}\nalbum {album}\nlength {length}\nposition {position}\npercentage {perc:.2}%\nlyric {last_text}");
+                let paragraph = Paragraph::new(to_print).block(block).scroll((scroll, 0));
                 f.render_widget(paragraph, size);
             }
         })?;
 
         thread::sleep(Duration::from_millis(100));
     }
+}
+
+async fn downloadLyrics () -> Result<lyric_finder::LyricResult, Box<dyn std::error::Error>> {
+    let client =  lyric_finder::Client::new();
+    let result = client.get_lyric("Epica - Storm the sorrow").await?;
+    match result {
+        lyric_finder::LyricResult::Some {
+            track,
+            artists,
+            lyric,
+        } => {
+            // println!("{} by {}'s lyric:\n{}", track, artists, lyric);
+            Ok(lyric_finder::LyricResult::Some {
+                track,
+                artists,
+                lyric,
+            })
+        }
+        lyric_finder::LyricResult::None => {
+            // println!("lyric not found!");
+            panic!("lyric not found!")
+        }
+    }
+}
+
+
+fn main() -> io::Result<()> {
+    main1();
+    Ok(())
 }
