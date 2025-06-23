92746165da40469a3d7b13d1a8f1800e894882cc
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-06-23 23:43:40 +0200

    Added sh script for "releasing" in dotfiles

---
+++ build_and_dotfile_it.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script builds galdrar and then copies the exec to ~/.config/niri
+
+cargo build --release;
+cp ~/Repositories/ratatoskr/target/release/galdrar ~/.config/niri/;
+echo -e "\n\033[0;32m\033[1mgaldrar built and copied to ~/.config/niri\033[0m";
