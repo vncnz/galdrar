325a8e6fbdbcf9c3cbf2d10e3256d7f866174150
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-11-25 19:15:49 +0100

    Added cached text rendering

--- src/lyrics.rs
+++ src/lyrics.rs
@@ -11,14 +11,16 @@ pub struct LyricLine {
 
 pub struct Lyrics {
     pub lines: Vec<LyricLine>,
-    pub rendered_text: Vec<ratatui::text::Line<'static>>
+    pub rendered_text: Vec<ratatui::text::Line<'static>>,
+    pub rendered_index: usize
 }
 
 impl Lyrics {
     pub fn new() -> Self {
         Self { 
             lines: vec![],
-            rendered_text: vec![]
+            rendered_text: vec![],
+            rendered_index: 0
         }
     }
 
@@ -31,24 +33,31 @@ impl Lyrics {
     }
 
     // TODO: cache current_index and don't restyle all if not changed
-    fn style_text(&mut self, position_secs: f64) -> Vec<ratatui::text::Line<'static>> {
+    fn style_text(&mut self, position_secs: f64) -> Option<(Vec<ratatui::text::Line<'static>>, usize)> {
         let current_index = if let Some(i) = self.current_lyric_index(position_secs) { i } else { 0 };
-        let lines: Vec<Line> = self.lines.iter().enumerate().map(|(i, line)| {
-            let style = if i == current_index {
-                Style::default().fg(Color::Yellow).add_modifier(Modifier::BOLD)
-            } else {
-                Style::default()
-            };
-            Line::from(vec![
-                Span::raw(format!("{} ", to_human(line.seconds))),
-                Span::styled(line.lyrics.clone(), style),
-            ])
-        }).collect();
-        lines
+        if current_index != self.rendered_index {
+            let lines: Vec<Line> = self.lines.iter().enumerate().map(|(i, line)| {
+                let style = if i == current_index {
+                    Style::default().fg(Color::Yellow).add_modifier(Modifier::BOLD)
+                } else {
+                    Style::default()
+                };
+                Line::from(vec![
+                    Span::raw(format!("{} ", to_human(line.seconds))),
+                    Span::styled(line.lyrics.clone(), style),
+                ])
+            }).collect();
+            Some((lines, current_index))
+        } else {
+            None
+        }
     }
 
     pub fn update_style_text(&mut self, position_secs: f64) {
-        self.rendered_text = self.style_text(position_secs);
+        if let Some((t, idx)) = self.style_text(position_secs) {
+            self.rendered_text = t;
+            self.rendered_index = idx;
+        }
     }
 
     pub fn set_text(&mut self, lines: Vec<LyricLine>) {
